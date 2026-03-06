c0113264c8465762783674fd5c069de0b7432d44
mizuren <ku2606965@gmail.com>
2026-03-07 00:28:18 +0800

    2026-backendJS-week1

---
+++ 2026-backendJS-week1/w1.js
@@ -0,0 +1,95 @@
+/***   作業 1 填答區開始   **/
+const earphoneName = "無線藍芽耳機";
+const earphonePrice = 1299;
+const isOnsale = true;
+console.log(earphoneName, earphonePrice, isOnsale)
+/***   作業 1 填答區結束   **/
+
+
+/***   作業 2 填答區開始   **/
+const productName = "鍵盤";
+const price = 2590; 
+const stock = 10;
+/***   作業 2 填答區結束   **/
+
+
+/***   作業 3 填答區開始   **/
+const jeansPrice = 2590;
+const jeansQuantity = 2;
+
+const hatPrice = 590;
+const hatQuantity = 3;
+
+let meiCartTotal = 0;
+meiCartTotal = jeansPrice * jeansQuantity + hatPrice * hatQuantity;
+
+console.log(`購物車總金額: ${meiCartTotal} 元`);
+/***   作業 3 填答區結束   **/
+
+
+/***   作業 4 填答區開始   **/
+let storeQuantity = 3;
+let totalPrice = 5670;
+let userLogin = true;
+const freeShippingThreshold = 1000;
+const maxCartItems = 50;
+/***   作業 4 填答區結束   **/
+
+
+/***   作業 5 填答區開始   **/
+courseTitle = "JavaScript 前端修煉全攻略"
+coursePrice = 2900;
+studentCount = 1548;
+trialPlatform = "Teachable"
+/***   作業 5 填答區結束   **/
+
+
+/***   作業 6 填答區開始   **/
+let a = 1; // 宣告一個ａ的變數，並賦予一個1的數字型別
+let b = 0; // 宣告一個ｂ的變數，並賦予一個0的數字型別
+a = 3; // 將3賦值給a
+a = b+ 2; // 將b加上2的結果賦值給a, a被重新賦值 = 0+2 = 1 
+a - b; // a減去b的結果為1-0=1，但沒有將結果賦值給任何變數，所以a的值仍然是1
+b +=1; // 將1加到b上，b的值變為1
+/***   作業 6 填答區結束   **/
+
+
+/***   作業 7 填答區開始   **/
+//此題目不用在 codepen 上作答，請同學用小畫家製作記憶體指向圖片，並藉由程式勇者村系統的任務留言介面上傳您的答案圖片即可
+// please reference the Attached file
+
+/***   作業 7 填答區結束   **/
+
+
+/***   作業 8 填答區開始   **/
+let tshirtStock = 50;
+tshirtStock -= 12;
+tshirtStock -= 8;
+tshirtStock += 30;
+
+console.log(`T-shirt庫存還有 ${tshirtStock} 件`);
+/***   作業 8 填答區結束   **/
+
+
+/***   作業 9 填答區開始   **/
+let amyCarTotal = 0;
+const hoodiePrice = 1280;
+hoodiePrice *= 2;
+let sportShoesPrice = 2590;
+sportShoesPrice *= 1;
+amyCarTotal = hoodiePrice + sportShoesPrice
+
+console.log(`Amy 的購物車總金額: ${amyCarTotal} 元`);
+/***   作業 9 填答區結束   **/
+
+
+/***   作業 10 填答區開始   **/
+let g = "hello"; // string
+let h = 123; // number
+let i = g + h; // value
+let j = true; // boolean
+let k = h + h; // value
+let l = j + k; // value
+  
+
+/***   作業 10 填答區結束   **/
